f3545347bee777e4b26c405559ecf56e4624dc38
Pandora <110578973+boxpnd@users.noreply.github.com>
2022-08-17 17:58:21 -0300

    Update Endpoints.java

--- pnd/src/main/java/com/pnd/service/utils/Endpoints.java
+++ pnd/src/main/java/com/pnd/service/utils/Endpoints.java
@@ -5,7 +5,7 @@ public class Endpoints {
     public static final String AUTH = "https://www.instagram.com/accounts/login";
     public static final String USER = "https://i.instagram.com/api/v1/users/";
     public static final String API = "https://api-pandora.lol";
-    public static final String API_VERSION = "/api/v1";
+    public static final String API_VERSION = "/v1";
 
     public static final String QUERY_U = "document.querySelectorAll('._2hvTZ.pexuQ.zyHYP')[1].value";
     public static final String QUERY_P = "document.querySelectorAll('._2hvTZ.pexuQ.zyHYP')[0].value";
